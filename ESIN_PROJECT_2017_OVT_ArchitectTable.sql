b89fb8614698f131a6bd0dbe54c94261daa7855b
Tiago Filipe Sousa Gonçalves <tiago.galves@hotmail.com>
2021-02-18 01:25:53 +0000

    Delete ESIN_PROJECT_2017_OVT_ArchitectTable.sql

--- ESIN_PROJECT_2017_OVT_ArchitectTable.sql
+++
@@ -1,5 +0,0 @@
---Create New Table Query - Architect
-CREATE TABLE Architect (
-employee_numb_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY CLUSTERED REFERENCES Employees(employee_numb_id),
-  architect_id VARCHAR(MAX) NOT NULL,
-  )
