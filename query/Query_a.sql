595c80ac459ce611e41429d9d20d97392906fb24
Tiago Filipe Sousa Gonçalves <tiago.galves@hotmail.com>
2021-02-18 01:26:07 +0000

    Delete query directory

--- query/Query_a.sql
+++
@@ -1,5 +0,0 @@
---ESIN Final Project
---a. A customer contact list, with the name, address, phone number and email.
-
-SELECT customer_name AS Customer, customer_address AS [Customer Address], customer_fn AS [Phone Number], customer_email AS [Customer e-mail]
-FROM Projects
--- query/Query_j.sql
+++
@@ -1,9 +0,0 @@
---ESIN FINAL PROJECT
-/*j. A list of the projects involving more than 3 technical areas.*/
-
-SELECT project_id, COUNT(techical_area_id) AS [Number of Projects]
-
-FROM ProjectsHaveTechAreas
-
-GROUP BY project_id
-HAVING (COUNT(techical_area_id) > 3)
