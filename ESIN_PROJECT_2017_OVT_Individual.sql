13585be0b378f76930b02040838dc1c52ed06332
Tiago Filipe Sousa Gonçalves <tiago.galves@hotmail.com>
2021-02-18 01:26:58 +0000

    Delete ESIN_PROJECT_2017_OVT_Individual.sql

--- ESIN_PROJECT_2017_OVT_Individual.sql
+++
@@ -1,6 +0,0 @@
---Create Table Contractors_Individual
-CREATE TABLE Individual
-  (contractor_numb_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY CLUSTERED REFERENCES Contractors(contractor_numb_id),
-  ind_profnumber INT NOT NULL, 
-  ind_mobilephn VARCHAR(MAX) NOT NULL,
-  )
