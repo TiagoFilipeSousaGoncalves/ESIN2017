deb86a690a9025ecf9cd41e00699881128d23c5a
Tiago Filipe Sousa Gonçalves <tiago.galves@hotmail.com>
2021-02-18 01:27:04 +0000

    Delete ESIN_PROJECT_2017_OVT_Monitoring.sql

--- ESIN_PROJECT_2017_OVT_Monitoring.sql
+++
@@ -1,9 +0,0 @@
---Create Table Monitoring
-CREATE TABLE TechnicalAreas
-  (monitoring_numb_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY CLUSTERED,
-   mon_id AS 'MON' + RIGHT('00000000' + CAST(monitoring_numb_id AS VARCHAR(8)), 8) PERSISTED,
-   mon_progress FLOAT NOT NULL, 
-   mon_workquality FLOAT NOT NULL, 
-   mon_initestbud FLOAT NOT NULL, 
-   mon_statusbud VARCHAR(MAX) NOT NULL
-  )
